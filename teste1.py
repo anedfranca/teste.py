632e1958cfe41f878c9970afe770ed1bcca25772
Ane França <127628489+anedfranca@users.noreply.github.com>
2023-08-27 16:06:02 -0300

    Update teste1.py

--- teste1.py
+++ teste1.py
@@ -1,7 +1,10 @@
 familia = ["Camilo", "Maria", "Ane", "Luisa"]
 idade_familia = [23, 20, 19, 15]
-#print(idade_familia)
-#idade_familia.sort() #ordenar crescente int idade
-#print(idade_familia)
-#familia.sort() #ordem alfabetica string
+
+print(idade_familia)
+
+idade_familia.sort() '''ordenar crescente int idade'''
+print(idade_familia)
+
+familia.sort() '''ordem alfabetica string'''
 print(familia)
